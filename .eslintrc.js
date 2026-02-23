1ec0aa535d1ec6b1fc9f7409ab73081917059b9a
Chuanyin1202 <alexabc@gmail.com>
2026-02-23 21:27:18 +0800

    Add devDependencies and project configuration files
    Install typescript, ts-jest, supertest, sqlite3, eslint, prettier,
    sequelize-cli, nodemon and type definitions. Add config files for
    ESLint, Prettier, Jest, TypeScript, nodemon, Sequelize CLI, and
    Docker Compose.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    jest: true,
+    es2020: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
