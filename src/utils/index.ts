28b9a37b4675f81f650018e5d7f59fbd2acc7f9e
Chuanyin1202 <alexabc@gmail.com>
2026-02-23 21:11:24 +0800

    Add password hashing and JWT utility functions

---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export { hashPassword, comparePassword } from './password';
+export { signToken, verifyToken } from './jwt';
---
+++ src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+import { appConfig } from '../config/app';
+
+interface TokenPayload {
+  userId: string;
+  email: string;
+}
+
+export function signToken(payload: TokenPayload): string {
+  return jwt.sign(payload, appConfig.jwt.secret, {
+    expiresIn: appConfig.jwt.expiresIn,
+  });
+}
+
+export function verifyToken(token: string): TokenPayload {
+  return jwt.verify(token, appConfig.jwt.secret) as TokenPayload;
+}
